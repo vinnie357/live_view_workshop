176dbb39a724199953b008d36e4751181102efc9
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2022-04-30 15:16:01 -0400

    Generate LiveHelper with modal from phx

--- lib/live_view_workshop_web.ex
+++ lib/live_view_workshop_web.ex
@@ -91,6 +91,7 @@ defmodule LiveViewWorkshopWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import LiveViewWorkshopWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
